d1e38c7cc634144d4ce6671b17e7cfb768e24eda
Dmitry <sypporf@gmail.com>
2017-06-20 22:34:05 +0300

    Add files via upload

---
+++ count_words.py
@@ -0,0 +1,16 @@
+﻿# -*- coding: utf-8 -*- 
+
+f = open('texts_for_tests\\bit.txt', 'r')
+s = f.read()
+s = s.replace(',', '')
+s = s.replace('.', '')
+sp = s.lower().split(' ')
+count = 0
+for a in sp:
+    
+    if a == 'и':
+        count = count + 1
+
+print(sp)
+
+input()
