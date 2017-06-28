49b68448efbe37846b6601fafd451c648e451f0b
Dmitry <sypporf@gmail.com>
2017-06-28 02:34:09 +0200

    Add files via upload
    it is worked

--- count_words.py
+++ count_words.py
@@ -2,6 +2,8 @@
 
 f = open('texts_for_tests\\bit.txt', 'r')
 s = f.read()
+f.close()
+
 s = s.replace(',', '')
 s = s.replace('.', '')
 s = s.replace(';', '')
@@ -11,15 +13,21 @@ s = s.replace('(', '')
 s = s.replace(')', '')
 s = s.replace('?', '')
 s = s.replace('!', '')
+s = s.replace('\n', '')
+s = s.replace('"', '')
+
 sp = s.lower().split(' ')
+print('Подготовка успешно')
 count = 0
 d = {}
 
 for i in sp:
+    print(count, 'из ', len(sp))
+    count = count + 1
     if i in d:
         pass
     else:
-        d[i] = 1
+        d[i] = 0
         for a in sp:
             if a == i:
                 d[i] = d[i] + 1
@@ -27,19 +35,24 @@ for i in sp:
 
 lkey = []
 lvalue = []
-
+print('Посчитали!')
 for t in d:
     lkey.append(t)
     lvalue.append(d[t])
     #print(t, ' ', d[t])
-
+print('сортируем...')
+c = 0
 for i in range(len(lvalue)):
+    print(c, 'из ', len(lvalue))
+    c = c + 1
     for j in range(len(lvalue) - 1, i, -1):
         if lvalue[j] < lvalue[j-1]:
             lvalue[j], lvalue[j-1] = lvalue[j-1], lvalue[j] 
             lkey[j], lkey[j-1] = lkey[j-1], lkey[j]
-    
+print('Сохраняем...')    
+f1 = open('texts_for_tests\\result.txt', 'w')
 for a in range(len(lkey)):
-    print(lkey[a], ': ', lvalue[a])
-
+    #print(lkey[a], ': ', lvalue[a])
+    f1.write(str(lkey[a]) + ': ' + str(lvalue[a]) + '\n')
+f1.close()    
 input()
